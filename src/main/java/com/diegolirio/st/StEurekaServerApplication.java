ddf253151f04dfb6e8b5999cbf0d7cbabcb39a3d
Diego Lirio <diegolirio.dl@gmail.com>
2017-11-08 02:27:28 -0200

    Eureka OK

---
+++ src/main/java/com/diegolirio/st/StEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.diegolirio.st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class StEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StEurekaServerApplication.class, args);
+	}
+}
